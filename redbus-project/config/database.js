c78698f8ad85b05d9fd6faa8fd6cbc8cdd27c1f2
hemachandran9944 <hemachandranhema8754@gmail.com>
2026-05-06 16:14:06 +0530

    Fix: MongoDB connection timeout issue and updated connection string

--- redbus-project/config/database.js
+++ redbus-project/config/database.js
@@ -1,16 +1,17 @@
+
+
 const mongoose = require('mongoose');
 
-const Cluster_DataBase = async () => {
-    try {
-        const cluster_databaseing = process.env.MONGO_URI || 'mongodb://hemachandranhema8754_db_user:F09oZcrRvHjg9hvj@ac-cduwql2-shard-00-00.62bc372.mongodb.net:27017,ac-cduwql2-shard-00-01.62bc372.mongodb.net:27017,ac-cduwql2-shard-00-02.62bc372.mongodb.net:27017/RedBus?ssl=true&replicaSet=atlas-fmbi4s-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-        await mongoose.connect(cluster_databaseing);
-        console.log('Cluster DataBase Connected Successfully ');
+const cluster_databseing = process.env.MONGO_URI || 'mongodb://hemachandranhema8754_db_user:F09oZcrRvHjg9hvj@ac-cduwql2-shard-00-00.62bc372.mongodb.net:27017,ac-cduwql2-shard-00-01.62bc372.mongodb.net:27017,ac-cduwql2-shard-00-02.62bc372.mongodb.net:27017/RedBus?ssl=true&replicaSet=atlas-fmbi4s-shard-0&authSource=admin&retryWrites=true&w=majority';
+mongoose.connect(cluster_databseing)
 
-    } catch (error) {
-        console.log('DataBase Connection Failed ', error.message);
-        process.exit(1); // stop server if DB fails
-    }
-};
+    .then(()=>{
+        console.log("Sueccessfulley Connecting Cluster Data Base!");
+    })
 
-module.exports = Cluster_DataBase;
+
+    .catch((error)=>{
+        console.log("falied Connecting Cluster Data Base!",error.message  );
+    });
+
